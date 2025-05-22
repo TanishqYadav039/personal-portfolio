e8a13c499c5e7f62323159d178c06d37946220a6
TanishqYadav039 <tanishqyadav039@gmail.com>
2025-05-22 22:13:44 +0530

    first project attach

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const Card = ({className, projectName, description}) => {
+const Card = ({projectimg, className, projectName, description}) => {
     return (
         <>
             <div className={className}>
-                <div className='w-[98%] h-1/2 border-2 flex items-center justify-center'>
-                    <img src="https://img.freepik.com/free-vector/realistic-weather-widget_1284-4092.jpg?ga=GA1.1.506606616.1746703658&semt=ais_hybrid&w=740" alt="project-1" className='w-full h-full object-fill 2xl:text-2xl' />
+                <div className='w-[98%] h-1/2 border-2 rounded-lg flex items-center justify-center'>
+                    <img src={projectimg ? projectimg : "https://img.freepik.com/free-vector/realistic-weather-widget_1284-4092.jpg?ga=GA1.1.506606616.1746703658&semt=ais_hybrid&w=740"} alt="project-1" className='w-full h-full object-cover 2xl:text-2xl rounded-lg' />
                 </div>
-                <h3 className='py-2 text-xl px-2 text-start w-full 2xl:text-3xl'>{projectName}</h3>
-                <p className='text-sm font-medium px-2 text-wrap 2xl:text-xl'>{description}</p>
+                <h3 className='py-2 text-2xl px-2 text-start w-full 2xl:text-3xl'>{projectName}</h3>
+                <p className='h-32 text-sm lg:text-lg overflow-y-scroll font-medium px-2 text-wrap 2xl:text-xl no-scrollbar'>{description}</p>
             </div>
         </>
     )
--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from '../Cards/Card'
+import ProjectImg1 from '../../assets/portfolioimg.png'
 
 const Project = () => {
     return (
@@ -10,27 +11,29 @@ const Project = () => {
                 <div className='w-[90%] grid grid-cols-1 grid-rows-3 md:grid-cols-2 md:grid-rows-2 xl:grid-rows-1 xl:grid-cols-3 justify-items-center items-center sm:w-[70%] md:w-[90%] 2xl:w-[95%] xl:w-full md:gap-8'>
 
                     <Card 
-                    className={'w-full h-[400px] 2xl:h-[600px] bg-[#90e0ef] px-3 flex flex-col justify-center items-center gap-2 font-bold my-4 md:w-[350px] 2xl:w-[500px] row-start-1 rounded-xl shadow-xl cursor-pointer hover:scale-102 transition duration-300 linear hover:shadow-cyan-400'}
-                    
-                    projectName={'Project-1'}
+                    projectimg={ProjectImg1}
 
-                    description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quibusdam vitae, magni eius eos quia quisquam nemo ad delectus repellendus maiores voluptatum nostrum. Mollitia, dolor.'} 
+                    className={'w-full h-[400px] 2xl:h-[600px] bg-[#90e0ef] px-3 flex flex-col justify-between items-center py-2 font-bold my-4 md:w-[350px] 2xl:w-[500px] row-start-1 rounded-xl shadow-xl cursor-pointer hover:scale-102 transition duration-300 linear hover:shadow-cyan-400'}
+                    
+                    projectName={"Personal portfolio"}
+
+                    description={'My personal portfolio built with reactjs and tailwindcss where i can focus to built responsive layout and visual design.'} 
                     />
                     
                     <Card 
-                    className={'w-full h-[400px] 2xl:h-[600px] bg-[#90e0ef] px-3 flex flex-col gap-2 justify-center items-center font-bold my-4 md:w-[350px] 2xl:w-[500px] row-start-2 md:row-start-1 md:col-start-2 rounded-xl shadow-xl cursor-pointer hover:scale-102 transition duration-300 linear hover:shadow-cyan-400'}
+                    className={'w-full h-[400px] 2xl:h-[600px] bg-[#90e0ef] px-3 flex flex-col justify-between items-center py-2 font-bold my-4 md:w-[350px] 2xl:w-[500px] row-start-1 rounded-xl shadow-xl cursor-pointer hover:scale-102 transition duration-300 linear hover:shadow-cyan-400'}
                     
                     projectName={'Project-2'}
 
-                    description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quibusdam vitae, magni eius eos quia quisquam nemo ad delectus repellendus maiores voluptatum nostrum. Mollitia, dolor.'}
+                    description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quibusdam vitae, magni eius eos quia quisquam nemo ad delectus dolor.'}
                     />
 
                     <Card 
-                    className={'w-full h-[400px] 2xl:h-[600px] bg-[#90e0ef] px-3 flex flex-col gap-2 justify-center items-center font-bold my-4 md:w-[350px] 2xl:w-[500px] row-start-3 md:row-start-2 md:col-start-1  md:col-span-2 xl:row-start-1 xl:col-start-3 xl:col-span-1 rounded-xl shadow-xl cursor-pointer hover:scale-102 transition duration-300 linear hover:shadow-cyan-400'}
+                    className={'w-full h-[400px] 2xl:h-[600px] bg-[#90e0ef] px-3 flex flex-col justify-between items-center py-2 font-bold my-4 md:w-[350px] 2xl:w-[500px] row-start-1 rounded-xl shadow-xl cursor-pointer hover:scale-102 transition duration-300 linear hover:shadow-cyan-400'}
 
                     projectName={'Project-3'}
 
-                    description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quibusdam vitae, magni eius eos quia quisquam nemo ad delectus repellendus maiores voluptatum nostrum. Mollitia, dolor.'}
+                    description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quibusdam vitae, magni eius eos quia quisquam nemo ad delectus dolor.'}
                     />
                     
 
